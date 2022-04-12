1dbdfc3356860ee12fef4ac3b3c3086efd91d1cf
cucgabrielpaul <cucgabrielpaul@yahoo.com>
2022-04-12 19:42:23 +0300

    Added text highlight style on repo search

--- src/components/RepoList/RepoItem.js
+++ src/components/RepoList/RepoItem.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Highlighter from "react-highlight-words";
 
 export default function RepoItem(props) {
-  const { name } = props;
+  const { name, filterQuery } = props;
   const router = useRouter();
 
   const handleShowRepoDetails = () => {
     router.push(`${router.asPath}/${name}`);
   };
-  return <li onClick={handleShowRepoDetails}>{name}</li>;
+  console.log(Highlighter);
+  return (
+    <li onClick={handleShowRepoDetails}>
+      <Highlighter
+        highlightClassName="YourHighlightClass"
+        searchWords={[filterQuery]}
+        autoEscape={true}
+        textToHighlight={name}
+      />
+    </li>
+  );
 }
