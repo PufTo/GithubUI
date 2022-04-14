0841c2fa6ba03ef35fafe3e90365a23754792ac3
cosminamosoiu <cosminutza.t@gmail.com>
2022-04-14 13:24:36 +0300

    Repolist styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import { theme } from '../styles/theme';
 import { ThemeProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />;
+      <CssBaseline />
     </ThemeProvider>
   );
 }
